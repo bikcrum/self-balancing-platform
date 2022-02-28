5f384a35cc789ccd9df7f38e24deecad8cf06772
bikram <bikcrum@gmail.com>
2022-02-27 21:21:58 -0800

    add balance on hand with reporting

--- plotter.py
+++ plotter.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import json
 import numpy as np
 
-data = json.load(open('reports/flat-balance-rewards.txt', 'r'))
+data = json.load(open('reports/hand-balance-rewards.txt', 'r'))
 data = np.array(data)
 plt.plot(data.sum(axis=1))
 plt.title('Training episode vs rewards\n4000 steps/episode, Max reward=1/step')
