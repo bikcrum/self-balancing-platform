7dbaa923ed9a7ee88c5e593a41db186b07d57f80
bikram <bikcrum@gmail.com>
2022-05-17 13:40:00 -0700

    update main.py

--- main.py
+++ main.py
@@ -1,14 +1,66 @@
-import gym
-# env = gym.make('CartPole-v1')
-from self_balancer import SelfBalancer
 from itertools import count
 
-env = SelfBalancer()
-env.reset()
+from mujoco_py import load_model_from_path, MjSim, MjViewer
+import numpy as np
+
+
+class SelfBalancerEnv:
+    def __init__(self):
+        model = load_model_from_path('selfblnc.xml')
+
+        self.sim = MjSim(model)
+        self.viewer = MjViewer(self.sim)
+
+        sim_state = self.sim.get_state()
+        self.sim.set_state(sim_state)
+
+    def step(self, action):
+        self.sim.data.ctrl[:] = action
+        self.sim.data.qpos[:] = [0, 0]
+        self.sim.step()
+
+        # observation, reward, done, info
+        return self._get_observation(), self._get_reward(), False, {}
+
+    def _get_observation(self):
+        # pos_x, pos_y, vel_x, vel_y
+        ob = self.sim.get_state()
+        ob = np.concatenate((ob.qpos, ob.qvel))
+        return ob
+
+    # how far from upright position
+    def _get_reward(self):
+        return 1 - sum(np.abs(self._get_observation()[:1]))
+
+    def render(self):
+        self.viewer.render()
+
+
+env = SelfBalancerEnv()
+
+motor_x = 0
+motor_y = 0
 for i in count():
     env.render()
-    observation, reward, done, info = env.step([1 if i % 50 < 25 else -1, 0])
-    # observation, reward, done, info = env.step(env.action_space.sample())
-    action_space = env.action_space
+
+    observation, reward, done, info = env.step([motor_x, motor_y])
+
+    # PROBLEM
+    # A simple PID controller didn't work due to inertial force in mujoco
+    # BUT hardware has enough damping/stopper in servo motor that let state to hold at one position
+    #
+    # SOLUTION
+    # 1. Model a servo motor in mujoco OR
+    # 2. Change servo motor to freely rotate and use torque instead of angle inputs
+
+    if observation[0] < 0:
+        motor_x += 1
+    else:
+        motor_x += -1
+
+    if observation[1] < 0:
+        motor_y += 1
+    else:
+        motor_y += -1
+
     print(reward)
-env.close()
